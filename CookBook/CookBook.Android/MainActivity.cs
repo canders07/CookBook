7c3875d5f4d8172e76a927d36f00790b0b19fb6c
Cullen Anderson <cullen.j.anderson07@gmail.com>
2018-12-24 14:59:19 -0700

    icons and such

--- CookBook/CookBook.Android/MainActivity.cs
+++ CookBook/CookBook.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CookBook.Droid
 {
-    [Activity(Label = "CookBook", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CookBook", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
